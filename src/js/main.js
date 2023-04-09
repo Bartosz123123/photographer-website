cb59ec0826845081c2bf70f9e53bc96bf0ee2dad
Bartosz <bartosz.opalka10@wp.pl>
2023-04-09 16:37:12 +0200

    new section started

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,23 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const dropdown = document.querySelector('.dropdown');
+const arrow = document.querySelector('.icon');
 
 const handleNav = () => {
 	dropdown.classList.toggle('active');
 };
 
+const handleArrowOnScroll = () => {
+	const top = window.scrollY;
+	if (top > 80) {
+		arrow.classList.add('hide-arrow');
+		arrow.classList.remove('show-arrow');
+	} else if (top < 45) {
+		arrow.classList.add('show-arrow');
+		arrow.classList.remove('hide-arrow');
+	}
+
+	console.log(top);
+};
+
+document.addEventListener('scroll', handleArrowOnScroll);
 burgerBtn.addEventListener('click', handleNav);
