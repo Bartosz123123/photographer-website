f850f373fa6c292a6d7cd8a72159df80ac26ce24
Bartosz <bartosz.opalka10@wp.pl>
2023-04-16 17:57:04 +0200

    home new changes

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,43 @@ const showAllBtn = document.querySelector('.show-all');
 const gallery = document.querySelector('.gallery');
 const slider = document.querySelector('.slider');
 
+const thumbnails = document.querySelectorAll('.gallery-img');
+const popup = document.querySelector('.popup');
+const closePopupBtn = document.querySelector('.close-popup');
+const imgInPopup = document.querySelector('.popup-img');
+const popupArrowLeft = document.querySelector('.popup-arrow-left');
+const popupArrowRgiht = document.querySelector('.popup-arrow-right');
+const body = document.querySelector('.body');
+let currentImageIndex;
+
+const handleRightArrowPopup = () => {
+	if (currentImageIndex === thumbnails.length - 1) {
+		currentImageIndex = 0;
+	} else {
+		currentImageIndex++;
+	}
+	imgInPopup.src = thumbnails[currentImageIndex].src;
+};
+
+const handleLeftArrowPopup = () => {
+	if (currentImageIndex === 0) {
+		currentImageIndex = thumbnails.length - 1;
+	} else {
+		currentImageIndex--;
+	}
+	imgInPopup.src = thumbnails[currentImageIndex].src;
+};
+
+const closePopup = () => {
+	popup.classList.add('popup-fade-out');
+	body.classList.remove('body-hidden');
+
+	setTimeout(() => {
+		popup.classList.add('popup-hidden');
+		popup.classList.remove('popup-fade-out');
+	}, 300);
+};
+
 const swapGalleryAndCarousel = () => {
 	gallery.classList.toggle('show-gallery');
 	slider.classList.toggle('hide-carousel');
@@ -81,8 +118,35 @@ window.addEventListener('scroll', () => {
 	});
 });
 
+document.addEventListener('keydown', (e) => {
+	if (!popup.classList.contains('popup-hidden')) {
+		if (e.key === 'ArrowRight' || e.keyCode === 39) {
+			handleRightArrowPopup();
+		} else if (e.key === 'ArrowLeft' || e.keyCode === 37) {
+			handleLeftArrowPopup();
+		} else if (e.key === 'Escape' || e.keyCode === 27) {
+			closePopup();
+		}
+	}
+});
+
+thumbnails.forEach((thumbnail, index) => {
+	thumbnail.addEventListener('click', (e) => {
+		popup.classList.remove('popup-hidden');
+		imgInPopup.src = e.target.src;
+		body.classList.add('body-hidden');
+		currentImageIndex = index;
+	});
+});
+
+popupArrowRgiht.addEventListener('click', handleRightArrowPopup);
+popupArrowLeft.addEventListener('click', handleLeftArrowPopup);
+closePopupBtn.addEventListener('click', closePopup);
 showAllBtn.addEventListener('click', swapGalleryAndCarousel);
 leftBtn.addEventListener('click', handleLeftArrow);
 rightBtn.addEventListener('click', handleRightArrow);
 document.addEventListener('scroll', handleArrowOnScroll);
 burgerBtn.addEventListener('click', handleNav);
+popup.addEventListener('click', (e) =>
+	e.target === popup ? closePopup() : false
+);
